spatial stream-debugged only: bugs fixed: numerical instability (softmax>clipping>crossentropy), exploading gradient problem. Adding EarlyStopping and TerminateOnNaN for debugging, prediction on test set, confusion matrix, manual tests (commented) for loading model and manual examination of the predictions

main.py
@@ -9,58 +9,58 @@ from glob import glob
 import collections
 import re
 import random
-SEED = 5 # set to None to use the current system time
+SEED = None # set to None to use the current system time
 random.seed(a=SEED)
 
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
 
 import tensorflow
 from tensorflow import keras
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, TimeDistributed, Dense, Dropout, Flatten, \
-    Activation, Conv2D, MaxPooling2D, LSTM, GlobalAveragePooling1D, average
+    Activation, Conv2D, MaxPooling2D, LSTM, GlobalAveragePooling1D, average, \
+    BatchNormalization, Masking, multiply, GlobalMaxPooling1D, Reshape, Lambda, GRU
+
 from tensorflow.keras.backend import clear_session
-from tensorflow.keras.optimizers import SGD
+from tensorflow.keras import optimizers
+from tensorflow.keras.callbacks import EarlyStopping, TerminateOnNaN
 
 # LABELS_REGEX = dataset.LABELS_REGEX
 LABELS_REGEX = dataset.PAPER_LABELS_REGEX
 CLASSES_N = len(LABELS_REGEX)
+
+KERAS_EPSILON = tensorflow.keras.backend.epsilon()
 
 keras.backend.set_image_data_format('channels_last')
 
 
 def build_model(model_dir, optimizer="adam"):
     spatial_input = Input(shape=(None, 16, 16, 1))
-    spatial_conv1 = TimeDistributed(Conv2D(32, (3, 3), padding='same', activation='relu'))(spatial_input)
-    spatial_pooled1 = TimeDistributed(MaxPooling2D(pool_size=(2, 2)))(spatial_conv1)
-    spatial_conv2 = TimeDistributed(Conv2D(32, (3, 3), activation='relu'))(spatial_pooled1)
-    spatial_dropout1 = TimeDistributed(Dropout(0.25))(spatial_conv2)
-    spatial_flatten1 = TimeDistributed(Flatten())(spatial_dropout1)
-    spatial_dense1 = TimeDistributed(Dense(512))(spatial_flatten1)
-    spatial_dense2 = TimeDistributed(Dense(35))(spatial_dense1)
-    spatial_LSTM1 = LSTM(CLASSES_N, return_sequences=True)(spatial_dense2)
-    spatial_glob_avg = GlobalAveragePooling1D()(spatial_LSTM1)
-    spatial_stream = spatial_glob_avg
-    # spatial_stream = Model(spatial_input, spatial_glob_avg)
-
     temporal_input = Input(shape=(None, 16, 16, 2))
-    temporal_conv1 = TimeDistributed(Conv2D(32, (3, 3), padding='same', activation='relu'))(temporal_input)
-    temporal_pooled1 = TimeDistributed(MaxPooling2D(pool_size=(2, 2)))(temporal_conv1)
-    temporal_conv2 = TimeDistributed(Conv2D(32, (3, 3), activation='relu'))(temporal_pooled1)
-    temporal_dropout1 = TimeDistributed(Dropout(0.25))(temporal_conv2)
-    temporal_flatten1 = TimeDistributed(Flatten())(temporal_dropout1)
-    temporal_dense1 = TimeDistributed(Dense(512))(temporal_flatten1)
-    temporal_dense2 = TimeDistributed(Dense(35))(temporal_dense1)
-    temporal_LSTM1 = LSTM(CLASSES_N, return_sequences=True)(temporal_dense2)
-    temporal_glob_avg = GlobalAveragePooling1D()(temporal_LSTM1)
-    temporal_stream = temporal_glob_avg
-    # temporal_stream = Model(temporal_input, temporal_glob_avg)
 
-    merged = average([spatial_stream, temporal_stream])
-    model = Model([spatial_input, temporal_input], merged)
+    spatial_conv1 = TimeDistributed(Conv2D(16, (3, 3), padding='same', activation='relu'))(spatial_input)
+    spatial_bn_layer = TimeDistributed(BatchNormalization())(spatial_conv1)
+    spatial_maxpool1 = TimeDistributed(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))(spatial_bn_layer)
+    spatial_conv2 = TimeDistributed(Conv2D(32, (3, 3), padding='same', activation='relu'))(spatial_maxpool1)
+    spatial_maxpool2 = TimeDistributed(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))(spatial_conv2)
+    spatial_conv3 = TimeDistributed(Conv2D(64, (3, 3), padding='same', activation='relu'))(spatial_maxpool2)
+    spatial_maxpool3 = TimeDistributed(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))(spatial_conv3)
+    spatial_conv4 = TimeDistributed(Conv2D(128, (3, 3), padding='same', activation='relu'))(spatial_maxpool3)
+    spatial_maxpool4 = TimeDistributed(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))(spatial_conv4)
+    spatial_flattened = TimeDistributed(Flatten())(spatial_maxpool4)
+    spatial_dense1 = TimeDistributed(Dense(512))(spatial_flattened)
+    spatial_dense2 = TimeDistributed(Dense(256))(spatial_dense1)
+    spatial_LSTM = LSTM(CLASSES_N, return_sequences=True, activation='softmax')(spatial_dense2)
+    spatial_global_pool = GlobalAveragePooling1D()(spatial_LSTM)
+    
+    #handle numerical instability
+    output = Lambda(lambda x: tensorflow.keras.backend.clip(x, KERAS_EPSILON, 1-KERAS_EPSILON))(spatial_global_pool)
 
+    model=Model([spatial_input, temporal_input], output)
     model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+    #model.compile(loss=tensorflow.losses.sigmoid_cross_entropy, optimizer=optimizer, metrics=['accuracy'])
 
     prepare.ensure_dir_exists(model_dir)
     keras.utils.plot_model(model, os.path.join(model_dir, 'model.png'))
@@ -72,15 +72,16 @@ def plot_history(history, model_dir):
     plt.title('model accuracy')
     plt.ylabel('accuracy')
     plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
+    plt.legend(['train', 'validation'], loc='upper left')
     plt.savefig(os.path.join(model_dir, "model_accuracy.png"))
+    plt.close()
 
     plt.plot(history.history['loss'])
     plt.plot(history.history['val_loss'])
     plt.title('model loss')
     plt.ylabel('loss')
     plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
+    plt.legend(['train', 'validation'], loc='upper left')
     plt.savefig(os.path.join(model_dir, "model_loss.png"))
     return
 
@@ -123,7 +124,7 @@ class DataGenerator(keras.utils.Sequence):
         flow_length_max = 0
         for idx in indices:
             [temperature_fn, flow_fn], y = self.data[idx]
-            temperature = np.load(temperature_fn)
+            temperature = np.load(temperature_fn).astype(np.float32)
             temperature = temperature[..., np.newaxis]
             flow = np.load(flow_fn)
             if temperature.shape[0] > temperature_length_max:
@@ -152,7 +153,7 @@ class DataGenerator(keras.utils.Sequence):
             return sequence
         trailing = np.zeros([length - sequence.shape[0], *sequence.shape[1:]],
                             sequence.dtype)
-        return np.vstack([sequence, trailing])
+        return np.vstack([trailing, sequence])
 
 
 if __name__ == "__main__":
@@ -181,7 +182,7 @@ if __name__ == "__main__":
                         help='How many epochs to run before ending.')
     parser.add_argument('--learning_rate',
                         type=float,
-                        default=0.01,
+                        default=1e-5,
                         help='How large a learning rate to use when training.')
     parser.add_argument(
         '--validation_size',
@@ -272,6 +273,7 @@ if __name__ == "__main__":
                 fn, y = sample
                 if (y == LABELS_REGEX[key]):
                     tmp_class.append(sample)
+            print("{} samples in class {}".format(len(tmp_class), LABELS_REGEX[key]))
             split = int(len(tmp_class) * FLAGS.validation_size)
             validation_fns_y.extend(tmp_class[:split])
             training_fns_y.extend(tmp_class[split:])
@@ -305,8 +307,8 @@ if __name__ == "__main__":
                                          FLAGS.batch_size,
                                          shuffle=True)
         testing_batches = DataGenerator(testing_data,
-                                      FLAGS.batch_size,
-                                      shuffle=True)
+                                      len(testing_data),
+                                      shuffle=False)
 
         print("[INFO] \n")
         print("Training: {} samples -> {} batches".format(
@@ -316,12 +318,65 @@ if __name__ == "__main__":
         print("Testing: {} samples -> {} batches".format(
             len(testing_data), len(testing_batches)))
         
-        optimizer = SGD(lr=1e-5)
+        optimizer = optimizers.SGD(lr=FLAGS.learning_rate, clipnorm=0.5)
         model = build_model(FLAGS.model_dir, optimizer)
         model.summary()
 
-        history = model.fit_generator(training_batches, epochs=FLAGS.epochs, validation_data=validation_batches)
+        # ! later change to val_los!! amd add to model.fig_generator
+        early_stopping = EarlyStopping(monitor='loss', patience=50, verbose=1)
+        terminateNaN = TerminateOnNaN()
+        history = model.fit_generator(training_batches, epochs=FLAGS.epochs, validation_data=validation_batches, callbacks=[early_stopping, terminateNaN])
         plot_history(history, FLAGS.model_dir)
-        
+        # ! test the model
+        # ! tensorboard
+        # ! saving is wrong
+        json_string = model.to_json()
+        model_fn_json = os.path.join(FLAGS.model_dir, "model.json")
+        model_fn_hdf5 = os.path.join(FLAGS.model_dir, "model.hdf5")
+        open(model_fn_json, 'w').write(json_string)
+        model.save_weights(model_fn_hdf5)
+        test = model.evaluate_generator(testing_batches)
+        print('Test loss:', test[0])
+        print('Test accuracy:', test[1])
+        predictions = model.predict_generator(testing_batches)
+        y_pred = np.argmax(predictions, axis=-1)
+        y_test = np.argmax(testing_batches[0][1], axis=-1)
+        cnfs_mtx = confusion_matrix(y_test, y_pred)
         clear_session()
         break
+
+'''
+manual testing
+# load json and create model
+json_file = open(model_fn_json, 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = tensorflow.keras.models.model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights(model_fn_hdf5)
+print("Loaded model from disk")
+walk = np.load(r"D:\tmps\cache\temperature\human1\walk_20170203_p5_dark1_126_142.npy")[..., np.newaxis]
+sitdown = np.load(r"D:\tmps\cache\temperature\human1\sitdown_20170203_p6_dark2_128_148.npy")[..., np.newaxis]
+standup = np.load(r"D:\tmps\cache\temperature\human1\standup_20170203_p8_light2_169_197.npy")[..., np.newaxis]
+falling = np.load(r"D:\tmps\cache\temperature\human1\falling1_20170203_p14_light1_104_125.npy")[..., np.newaxis]
+sit = np.load(r"D:\tmps\cache\temperature\human1\sit_20170203_p1_dark3_197_208.npy")[..., np.newaxis]
+lie = np.load(r"D:\tmps\cache\temperature\human1\lie_20170203_p3_light4_175_199.npy")[..., np.newaxis]
+stand = np.load(r"D:\tmps\cache\temperature\human1\stand_20170203_p3_light3_186_209.npy")[..., np.newaxis]
+
+predictions = []
+for action in [walk, sitdown, standup, falling, sit, lie, stand]:
+    predictions.append(model.predict([action[np.newaxis], np.random.rand(np.prod([*action.shape[:-1], 2])).reshape([*action.shape[:-1], 2])[np.newaxis]]))
+
+for action in predictions:
+    print(action.argmax())
+
+
+def __pad_to_length(sequence, length):
+    if sequence.shape[0] == length:
+        return sequence
+    trailing = np.zeros([length - sequence.shape[0], *sequence.shape[1:]],
+                        sequence.dtype)
+    return np.vstack([sequence, trailing])
+
+a = __pad_to_length(falling, 30)
+'''