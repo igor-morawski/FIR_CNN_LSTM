0562a13d0b9b1bb28949d0f8783dc3ede210d57e
unknown <igor.morawski@hotmail.com>
2019-08-22 16:00:57 +0800

    augmentation tools

--- tools/augmentation.py
+++ tools/augmentation.py
@@ -41,32 +41,3 @@ def random_flip(array):
         return np.flip(array, axis=2)
     else:
         return array
-
-import os
-walk =r"human1\walk_20170203_p5_dark1_126_142.npy"
-sitdown = r"human1\sitdown_20170203_p6_dark2_128_148.npy"
-standup = r"human1\standup_20170203_p8_light2_169_197.npy"
-falling = r"human1\falling1_20170203_p14_light1_104_125.npy"
-sit = r"human1\sit_20170203_p1_dark3_197_208.npy"
-lie = r"human1\lie_20170203_p3_light4_175_199.npy"
-stand = r"human1\stand_20170203_p3_light3_186_209.npy"
-predictions = []
-temperature_dir = "/" + os.path.join("tmps", "cache", "temperature")
-flow_dir = "/" + os.path.join("tmps", "cache", "optical_flow")
-
-
-for action in [walk, sitdown, standup, falling, sit, lie, stand]:
-    temp = np.load(os.path.join(temperature_dir, action))
-    temp = temp[..., np.newaxis]
-    temp = temp.reshape([-1, *temp.shape])
-    flow = np.load(os.path.join(flow_dir, action))
-    flow = flow.reshape([-1, *flow.shape])
-
-
-
-for action in [walk, sitdown, standup, falling, sit, lie, stand]:
-    temp = np.load(os.path.join(temperature_dir, action))
-    flow = np.load(os.path.join(flow_dir, action))
-
-a = temp
-b = flow
